86141ad7c86bb3503efa6fbfae7b200870ad071e
Eugene Dubovoy <eugene.dubovoy@gmail.com>
2016-10-21 00:11:27 +0300

    Add function calls ast visitor

---
+++ extdbg/func_calls.py
@@ -0,0 +1,42 @@
+""" Module used to get all function calls from a python file """
+import ast
+from collections import deque
+
+
+class FuncCallVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self._name = deque()
+
+    @property
+    def name(self):
+        return '.'.join(self._name)
+
+    @name.deleter
+    def name(self):
+        self._name.clear()
+
+    def visit_Name(self, node):
+        self._name.appendleft(node.id)
+
+    def visit_Attribute(self, node):
+        try:
+            self._name.appendleft(node.attr)
+            self._name.appendleft(node.value.id)
+        except AttributeError:
+            self.generic_visit(node)
+
+
+def get_func_calls(tree):
+    func_calls = []
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Call):
+            callvisitor = FuncCallVisitor()
+            callvisitor.visit(node.func)
+            func_calls.append(callvisitor.name)
+
+    return func_calls
+
+
+if __name__ == '__main__':
+    tree = ast.parse(open(__file__).read())
+    print get_func_calls(tree)
