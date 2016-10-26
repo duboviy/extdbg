5dcb0683a50b0921825e6e9cbdc751e8a503ce31
Eugene Dubovoy <eugene.dubovoy@gmail.com>
2016-10-26 13:36:22 +0300

    Minor license extension change and setup.py version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'extdbg',
   packages = ['extdbg'], # this must be the same as the name above
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'An extended debugging python utilities.',
   author = 'Eugene Duboviy',
   author_email = 'eugene.dubovoy@gmail.com',
   url = 'https://github.com/duboviy/extdbg', # use the URL to the github repo
-  download_url = 'https://github.com/duboviy/extdbg/tarball/0.0.1', # I'll explain this in a second
+  download_url = 'https://github.com/duboviy/extdbg/tarball/0.0.2', # I'll explain this in a second
   keywords = ['debug', 'utility', 'testing'], # arbitrary keywords
   classifiers=[
     "Programming Language :: Python",
